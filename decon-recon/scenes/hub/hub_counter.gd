8fcdec44ef47f1a93f8a98fe9b387930f2544119
O.T.A. Janssen <o.t.a.janssen@gmail.com>
2026-04-01 16:06:30 +0200

    Hub counter code and play button

--- decon-recon/scenes/hub/hub_counter.gd
+++ decon-recon/scenes/hub/hub_counter.gd
@@ -2,17 +2,22 @@ extends Control
 
 @export var marker_symbol: String
 @export var show_timer: bool
+@export var default_score: int
 
 var marker : Label
+var score : Label
 var timer : Label
 
-func _init(p_marker_symbol = "x", p_show_timer = false):
+func _init(p_marker_symbol = "x", p_show_timer = false, p_default_score = 0):
 	marker_symbol = p_marker_symbol
 	show_timer = p_show_timer
+	default_score = p_default_score
 
 func _ready() -> void:
 	marker = get_node("MarginContainer/VBoxContainer/PanelContainer/HBoxContainer/Marker")
 	timer = get_node("MarginContainer/VBoxContainer/Timer")
+	score = get_node("MarginContainer/VBoxContainer/PanelContainer/HBoxContainer/Score")
 	
 	marker.text = marker_symbol
 	timer.visible = show_timer
+	score.text = str(default_score)
